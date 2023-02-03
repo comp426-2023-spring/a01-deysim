974a35c02b0566a389beca08132f4fdbd2724c33
Deysi Morales Magadan <77804551+deysim@users.noreply.github.com>
2023-02-02 22:29:43 -0500

    fix syntax

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ fs.readFile('./public/index.html','utf8',(error,data) => {
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req,res) => {
   res.statusCode = 200;
-  res.setHeader(Content-Type,'text/html');
+  res.setHeader('Content-Type','text/html');
   res.end(x);
 });
 
